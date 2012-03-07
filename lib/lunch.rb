f78ba994c5c4ae3b03fc4b9d236434c9ac8167dd
Patrick Strang <strangnet@gmail.com>
2012-03-07 01:34:18 +0100

    Some boilerplate additions

---
+++ lib/lunch.rb
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class Lunch
+  def initialize
+    puts "Time for Lunch"
+  end
+end
---
+++ lib/main.rb
@@ -0,0 +1,3 @@
+require 'Lunch'
+
+lunch = Lunch.new
